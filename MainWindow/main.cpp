c1e5bbd25e3a2e356edd1f7bac799d47d2bf26bc
roman <roman@ROMANPC>
2026-01-06 16:55:03 +0300

    Calc Window DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,6 +1,9 @@
 ﻿//MainWindow
 #include <Windows.h>
+#include<cstdio>
 #include "resource.h"
+
+#define IDC_BUTTON 1000
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My first window";
 
@@ -48,14 +51,21 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		return 0;
 	}
     // 2. Создание окна.
+	int screen_width = GetSystemMetrics(SM_CXSCREEN); //Размер экрана.
+	int screen_height = GetSystemMetrics(SM_CYSCREEN); //Размер экрана.
+	int window_width = screen_width * 3 / 4;
+	int window_height = screen_height * 3 / 4;
+	int x_position = screen_width / 8;
+	int y_position = screen_height / 8;
+
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,  //exStyle - дополнительные стили.
 		g_sz_WINDOW_CLASS,  // Имя класса окна.
 		g_sz_WINDOW_CLASS,  // Заголовок окна.
 		WS_OVERLAPPEDWINDOW,//Стиль окна. Стили всезда зависят от класса окна.WS_OVERLAPPEDWINDOW - главное окно.
-		CW_USEDEFAULT, CW_USEDEFAULT, //Position - начальная позиция.
-		640, 480, //Размер окна.
+		x_position, y_position, //Position - начальная позиция.
+		window_width, window_height, //Размер окна.
 		NULL, // Наличие родителького окна.
 		NULL, // Для главного окна это ResourceID главного меню, для дочернего окна (Control) это ResourceID 
 		//дочернего окна, наример (IDC_BUTTON_COPY).
@@ -67,9 +77,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		MessageBox(NULL, "Windows creation failed", NULL, MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	ShowWindow(hwnd, nCmdShow); // Создает режим отображения окнаб например,"Развернуто на весь экран",
-	                            // "Свернуто в окно", "Свернуто на анель задач.
-	UpdateWindow(hwnd);         // Обновляет рабочую облать окна отправляя сообщение WM_PAINT, 
+	ShowWindow(hwnd, nCmdShow); // Создает режим отображения окна, например,"Развернуто на весь экран",
+	                            // "Свернуто в окно", "Свернуто на ппанель задач.
+	UpdateWindow(hwnd);         // Обновляет рабочую область окна отправляя сообщение WM_PAINT, 
 	                            // если рабочая область окна не пустая.
 
 	// 3. Запуск цикла сообщений.
@@ -87,9 +97,53 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
+	{
+		HWND hButton = CreateWindowEx
+		(
+			NULL,                   //exStyle
+			"Button",               //Class
+			"Кнопка",               //Title
+			WS_CHILD | WS_VISIBLE,  //Style
+			10, 10,                 //Position
+			150, 80,                //Size
+			hwnd,                   //Parent
+			(HMENU)IDC_BUTTON,     //Для главного окна это ResourceID главного меню, для дочернего окна (элемента)
+		//управления окна 'Control') это ResourceID дочернего окна, наример (IDC_BUTTON_COPY).
+			GetModuleHandle(NULL), //hInstence
+			NULL                   //???
+		);
+	}
 		break;
+	case WM_MOVE: //сообщение об изменении положения окна на экране
+	case WM_SIZE: // сообщение от изменении размера окна
+	{
+		RECT window_rect = {};  //функция, оределяющая рямоугольник окна
+		GetWindowRect(hwnd, &window_rect);
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE] = {}; //создаем буфер
+		wsprintf
+		(
+			sz_title,
+			"%s, Position: %ix%i, Size: %ix%i", // загоняем сюда информацию о прямоугольнике
+			g_sz_WINDOW_CLASS,
+			window_rect.left, window_rect.top, // позиция
+			window_rect.right - window_rect.left,
+			window_rect.bottom - window_rect.top //размеры окна
+
+		);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}
+	break;
 	case WM_COMMAND:
-		break;
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON:
+			MessageBox(hwnd, "Cursor check", "Info", MB_OK | MB_ICONINFORMATION);
+			break;
+		}
+	}
+	break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
